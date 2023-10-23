349267ca901aa8058cf100f9539095f43efb3f38
Sarah Allett <sarahallett555@gmail.com>
2023-10-23 18:07:46 +0100

    Adding comments

--- MessageGen.js
+++ MessageGen.js
@@ -12,16 +12,16 @@ const randomNumber = num => {
 }
 
 const messageGen = (startingMessage, signMessages, adjectiveMessages, verbMessage) => {
-
+    // Get random numbers
      const randomStartingMessage = randomNumber(startingMessage.length);
      const randomSignMessage = randomNumber(signMessages.length);
      const randomAdjectiveMessage = randomNumber(adjectiveMessages.length);
      const randomVerbMessage = randomNumber(verbMessage.length);
      
-     
+     //Get random message
      const startMessage = startingMessage[randomStartingMessage];
      let connectionMessage;
-
+    //Create random connection message
      if(startMessage.includes('sign')) {
         connectionMessage = signMessages[randomSignMessage];
      } else if (startMessage.includes('are')) {
