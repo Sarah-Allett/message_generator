8b175650217f6067e7dcd4eefc7a6b64d47b2e83
Sarah Allett <sarahallett555@gmail.com>
2023-10-23 18:06:15 +0100

    Adding functions

---
+++ MessageGen.js
@@ -0,0 +1,39 @@
+const startingMessage = ['Your sign is', 'You are', 'You should'];
+
+const signMessages = ['moon', 'sun', 'air', 'water'];
+
+const adjectiveMessages = ['kind and caring', 'ambitious', 'generous', 'sensitive and empathic'];
+
+const verbMessage = ['follow your dreams', 'take some self-care time', 'be kind to yourself'];
+
+
+const randomNumber = num => {
+    return Math.floor(Math.random() * num);
+}
+
+const messageGen = (startingMessage, signMessages, adjectiveMessages, verbMessage) => {
+
+     const randomStartingMessage = randomNumber(startingMessage.length);
+     const randomSignMessage = randomNumber(signMessages.length);
+     const randomAdjectiveMessage = randomNumber(adjectiveMessages.length);
+     const randomVerbMessage = randomNumber(verbMessage.length);
+     
+     
+     const startMessage = startingMessage[randomStartingMessage];
+     let connectionMessage;
+
+     if(startMessage.includes('sign')) {
+        connectionMessage = signMessages[randomSignMessage];
+     } else if (startMessage.includes('are')) {
+        connectionMessage = adjectiveMessages[randomAdjectiveMessage];
+     } else {
+        connectionMessage = verbMessage[randomVerbMessage];
+     }
+
+     let randomMessage = `${startMessage} ${connectionMessage}`;
+
+     console.log(randomMessage);
+}
+
+messageGen(startingMessage, signMessages, adjectiveMessages, verbMessage);
+
--- MessageGen.js.js
+++
@@ -1,8 +0,0 @@
-const startingMessage = ['Your sign is ', 'You are ', 'You should '];
-
-const signMessages = ['moon', 'sun', 'air', 'water'];
-
-const adjectiveMessages = ['kind and caring', 'ambitious', 'generous', 'sensitive and empathic'];
-
-const verbMessage = ['follow your dreams', 'take some self-care time', 'be kind to yourself'];
-
